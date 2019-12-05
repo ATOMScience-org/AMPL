d71c4851d725168989e453a2f22c806f563365e4
ATOM <support@atomscience.org>
2019-12-05 10:11:10 -0800

    Update conf.py

--- atomsci/ddm/docs/source/conf.py
+++ atomsci/ddm/docs/source/conf.py
@@ -179,4 +179,4 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
-autodoc_mock_imports = ["rdkit"]
+autodoc_mock_imports = ["rdkit", "tensorflow.contrib", "atomsci"]
