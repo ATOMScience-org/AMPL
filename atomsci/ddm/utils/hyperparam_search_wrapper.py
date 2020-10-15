05c6d1fa02d2c454c1ed1ff499411aabd846c6d2
Benjamin Madej <benjamin.madej@nih.gov>
2020-10-15 13:33:59 -0700

    Fix SLURM option parsing

--- atomsci/ddm/utils/hyperparam_search_wrapper.py
+++ atomsci/ddm/utils/hyperparam_search_wrapper.py
@@ -286,11 +286,11 @@ class HyperparameterSearch(object):
         with open(self.shell_script, 'w') as f:
             f.write("#!/bin/bash\n")
 
-            # If lc_account == 'None' then SLURM account is not set
-            if self.params.lc_account != 'None':
+            # If 'None' is in lc_account then SLURM account is not set
+            if not ('None' in self.params.lc_account):
                 f.write("#SBATCH -A {0}\n".format(self.params.lc_account))
-            # If slurm_partition == 'None' then SLURM partition is not set
-            if self.params.slurm_partition != 'None':
+            # If 'None' is in slurm_partition then SLURM partition is not set
+            if not ('None' in self.params.slurm_partition):
                 f.write("#SBATCH -p {0}\n".format(self.params.slurm_partition))
 
             f.write("#SBATCH -N 1\n#SBATCH -t {0}\n#SBATCH --export=ALL\n#SBATCH -D {1}\n".format(self.params.slurm_time_limit,
