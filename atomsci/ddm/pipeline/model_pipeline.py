50170f4f64f0e3790a239852536baef1c9167e05
He, Stewart <he6@llnl.gov>
2021-05-19 15:21:36 -0700

    proposed fix if pkg_resources fails to find a path

--- atomsci/ddm/pipeline/model_pipeline.py
+++ atomsci/ddm/pipeline/model_pipeline.py
@@ -25,14 +25,18 @@ import copy
 
 from atomsci.ddm.utils import datastore_functions as dsf
 
+import pkg_resources
 if 'site-packages' in dsf.__file__: # install_dev.sh points to github directory
     import subprocess
     import json
     data = subprocess.check_output(["pip", "list", "--format", "json"])
     parsed_results = json.loads(data)
     ampl_version=next(item for item in parsed_results if item["name"] == "atomsci-ampl")['version']
+elif pkg_resources.resource_filename('atomsci', '') is None:
+    f=open(dsf.__file__.rsplit('/', maxsplit=4)[0]+'/VERSION', 'r')
+    ampl_version = f.read().strip()
+    f.close()
 else: # install.sh points to installation directory
-    import pkg_resources
     VERSION_fn = os.path.join(
         os.path.dirname(pkg_resources.resource_filename('atomsci', '')),
         'VERSION')
