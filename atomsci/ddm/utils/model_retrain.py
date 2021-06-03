b5a61dc356732e94cf2c777549a4b883afe6c701
Amanda Paulson <amanda.paulson@nih.gov>
2021-06-02 19:25:59 -0700

    add retrain from tracker fcn

--- atomsci/ddm/utils/model_retrain.py
+++ atomsci/ddm/utils/model_retrain.py
@@ -33,6 +33,17 @@ import atomsci.ddm.pipeline.model_pipeline as mp
 import atomsci.ddm.pipeline.parameter_parser as parse
 import atomsci.ddm.utils.curate_data as curate_data
 import atomsci.ddm.utils.struct_utils as struct_utils
+import atomsci.ddm.pipeline.model_tracker as mt
+import atomsci.ddm.utils.datastore_functions as dsf
+
+mlmt_supported = True
+try:
+    from atomsci.clients import MLMTClient
+except (ModuleNotFoundError, ImportError):
+    logger.debug("Model tracker client not supported in your environment; will save models in filesystem only.")
+    mlmt_supported = False
+    
+
 
 def train_model(input, output):
     """ Retrain a model saved in a model_metadata.json file
@@ -59,7 +70,9 @@ def train_model(input, output):
     # use the same split
     params.previously_split = True
     params.split_uuid = config['splitting_parameters']['split_uuid']
-
+    # specify collection
+    
+    
     logger.debug("model params %s" % str(params))
 
     # Create model pipeline
@@ -92,6 +105,49 @@ def train_model_from_tar(input, output):
 
     return train_model(metadata_path, output)
 
+def train_model_from_tracker(model_uuid, output_dir):
+    """ Retrain a model saved in the model tracker
+
+    Args:
+        model_uuid (str): model tracker model_uuid file
+
+        output_dir (str): path to output directory
+
+    Returns:
+        None
+    """
+    
+    if not mlmt_supported:
+        print("Model tracker not supported in your environment; can load models from filesystem only.")
+        return None
+
+    mlmt_client = dsf.initialize_model_tracker()
+    
+    collection_name = mt.get_model_collection_by_uuid(model_uuid, mlmt_client=mlmt_client)
+        
+    # get metadata from tracker
+    config = mt.get_metadata_by_uuid(model_uuid)
+    
+    # Parse parameters
+    params = parse.wrapper(config)
+    params.result_dir = output_dir
+    # otherwise this will have the same uuid as the source model
+    params.model_uuid = None
+    # use the same split
+    params.previously_split = True
+    params.split_uuid = config['splitting_parameters']['split_uuid']
+    # specify collection
+    params.collection_name = collection_name
+    logger.debug("model params %s" % str(params))
+
+    # Create model pipeline
+    model = mp.ModelPipeline(params)
+
+    # Train model
+    model.train_model()
+
+    return model
+
 
 #----------------
 # main
@@ -101,7 +157,7 @@ def main(argv):
 
     # input file/dir (required)
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', required=True, help='input directory/file')
+    parser.add_argument('-i', '--input', required=True, help='input directory, file or model_uuid')
     parser.add_argument('-o', '--output', help='output result directory')
 
     args = parser.parse_args()
@@ -122,7 +178,10 @@ def main(argv):
     elif input.endswith('.tar.gz'):
         train_model_from_tar(input, output)
     else:
-        raise Exception('Unrecoganized input %s'%input)
+        try:
+            train_model_from_tracker(input, output)
+        except:
+            Exception('Unrecognized input %s'%input)
 
     elapsed_time_secs = time.time() - start_time
     logger.info("Execution took: %s secs" % timedelta(seconds=round(elapsed_time_secs)))
