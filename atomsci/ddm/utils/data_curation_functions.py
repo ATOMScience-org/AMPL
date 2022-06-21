b8dbef234280efed5f9997dda9883e1e9273d309
Kevin S. McLoughlin <mcloughlin2@llnl.gov>
2022-06-20 21:57:20 -0700

    Added back new parameters to standardize_relations() which somehow got lost in 1.4.0.

--- atomsci/ddm/utils/data_curation_functions.py
+++ atomsci/ddm/utils/data_curation_functions.py
@@ -90,7 +90,7 @@ def exclude_organometallics(df, smiles_col='rdkit_smiles'):
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-def standardize_relations(dset_df, db='DTC'):
+def standardize_relations(dset_df, db='DTC', rel_col=None, output_rel_col=None, invert=False):
     """ Standardizes censoring operators
 
     Standardize the censoring operators to =, < or >, and remove any rows whose operators
