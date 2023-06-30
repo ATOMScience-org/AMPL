7ced8ade3dd8bad679f18b08874a83d1eb7699f9
Kevin S. McLoughlin <mcloughlin2@llnl.gov>
2023-06-30 13:41:25 -0700

    Fixed freq_table's handling of nans in selected column.

--- atomsci/ddm/utils/curate_data.py
+++ atomsci/ddm/utils/curate_data.py
@@ -311,8 +311,11 @@ def freq_table(dset_df, column, min_freq=1):
             and the column 'Count'. The 'Count' column contains the number of occurances for each
             value in the 'column' argument.
     """
-    vals = dset_df[column].values
-    uniq_vals, counts = np.unique(vals, return_counts=True)
+    nmissing = sum(dset_df[column].isna())
+    filt_df = dset_df[dset_df[column].notna()]
+    uniq_vals, counts = np.unique(filt_df[column].values, return_counts=True)
+    uniq_vals = uniq_vals.tolist() + [np.nan]
+    counts = counts.tolist() + [nmissing]
     uniq_df = pd.DataFrame({column: uniq_vals, 'Count': counts}).sort_values(by='Count', ascending=False)
     uniq_df = uniq_df[uniq_df.Count >= min_freq]
     return uniq_df
