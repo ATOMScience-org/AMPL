53bfa9179e0d7a54b9e95e400606625949c3310d
Holger Jones <holgerjones@llnl.gov>
2023-10-24 11:22:01 -0700

    allow individual install of pyg suite components

--- install_pyg_rocm.sh
+++ install_pyg_rocm.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 ml rocm/5.4.3
-ml rocmcc/5.4.3-magic
+#ml rocmcc/5.4.3-magic
 ml python/3.9.12
 
-if [[ $# -eq 0 ]] ; then
-    echo 'Script needs as input a venv directory to activate'
+if [[ $# -ne 2 ]] ; then
+    echo 'Script needs as input two arguments'
+    echo 'a venv directory to activate; and one of cluster, scatter, sparse, spline, geometric, or full'
     exit 1
 fi
 
@@ -20,39 +21,49 @@ if [[ ! $LD_LIBRARY_PATH =~ "torch/lib" ]]; then
   export LD_LIBRARY_PATH=$torchlib:$LD_LIBRARY_PATH
 fi
 
+if [[ $2 =~ .*"cluster".* || $2 == "full" ]]; then
 pushd tpl/pyg-rocm-build/pytorch_cluster-1.6.1/
 echo "Building torch_cluster"
 python3 setup.py build_ext -j 24
 python3 setup.py bdist_wheel
 python3 -m pip install --no-deps --force-reinstall dist/torch_cluster*.whl
 popd
+fi
 
+if [[ $2 =~ .*"scatter".* || $2 == "full" ]]; then
 pushd tpl/pyg-rocm-build/pytorch_scatter-2.1.1/
 echo "Building torch_scatter"
 python3 setup.py build_ext -j 24
 python3 setup.py bdist_wheel
 python3 -m pip install --no-deps --force-reinstall dist/torch_scatter*.whl
 popd
+fi
 
+if [[ $2 =~ .*"sparse".* || $2 == "full" ]]; then
 pushd tpl/pyg-rocm-build/pytorch_sparse-0.6.17/
 echo "Building torch_sparse"
 python3 setup.py build_ext -j 24
 python3 setup.py bdist_wheel
 python3 -m pip install --no-deps --force-reinstall dist/torch_sparse*.whl
 popd
+fi
 
+if [[ $2 =~ .*"spline".* || $2 == "full" ]]; then
 pushd tpl/pyg-rocm-build/pytorch_spline_conv-1.2.2/
 echo "Building torch_spline_conv"
 python3 setup.py build_ext -j 24
 python3 setup.py bdist_wheel
 python3 -m pip install --no-deps --force-reinstall dist/torch_spline*.whl
 popd
+fi
 
+if [[ $2 =~ .*"geometric".* || $2 == "full" ]]; then
 pushd tpl/pyg-rocm-build/pytorch_geometric-2.3.1/
 echo "Building torch_geometric"
 python3 setup.py bdist_wheel
 python3 -m pip install --no-deps --force-reinstall dist/torch_geometric*.whl
 popd
+fi
 
 echo "Done Installing pyg!!"
 
