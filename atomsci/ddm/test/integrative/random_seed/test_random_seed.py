ae57f7f5cafaa9309c290647e1ba63d929ed2942
he6@llnl.gov <he6@llnl.gov>
2023-12-11 10:27:54 -0800

    added test for retraining a model with a fixed random seed

--- atomsci/ddm/test/integrative/random_seed/test_random_seed.py
+++ atomsci/ddm/test/integrative/random_seed/test_random_seed.py
@@ -8,10 +8,8 @@ import shutil
 import glob
 
 import atomsci.ddm.pipeline.model_pipeline as mp
-import atomsci.ddm.pipeline.predict_from_model as pfm
 import atomsci.ddm.pipeline.parameter_parser as parse
-import atomsci.ddm.utils.curate_data as curate_data
-import atomsci.ddm.utils.struct_utils as struct_utils
+import atomsci.ddm.utils.model_retrain as mr
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import integrative_utilities
@@ -119,6 +117,57 @@ def test_seeded_RF():
     # clean again
     clean()
 
+def test_seeded_RF_retrain():
+    # init training dataset
+    init()
+    # Train model
+    # -----------
+    # Read parameter JSON file
+    prefix = 'delaney-processed'
+    with open('reg_config_delaney_fit_RF_mordred_filtered.json') as f:
+        config = json.loads(f.read())
+
+    config['rf_random_seed'] = 0
+
+    # Parse parameters
+    params = parse.wrapper(config)
+
+    # Create model pipeline
+    model = mp.ModelPipeline(params)
+
+    # Train model
+    model.train_model()
+
+    # Get uuid and reload directory
+    # -----------------------------
+    model_type = params.model_type
+    prediction_type = params.prediction_type
+    descriptor_type = params.descriptor_type
+    featurizer = params.featurizer
+    splitter = params.splitter
+    model_dir = 'result/%s_curated_fit/%s_%s_%s_%s'%(prefix, model_type, featurizer, splitter, prediction_type)
+    uuid = model.params.model_uuid
+    tar_f = 'result/%s_curated_fit_model_%s.tar.gz'%(prefix, uuid)
+    reload_dir = model_dir+'/'+uuid
+
+    # check saved scores
+    original_score = integrative_utilities.read_training_statistics_file(reload_dir, 'test', 'r2_score')
+
+    retrained_model = mr.train_model_from_tar(input=tar_f, output='result')
+    retrained_model_dir = model_dir
+    retrained_uuid = retrained_model.params.model_uuid
+    retrained_reload_dir = os.path.join(retrained_model_dir, retrained_uuid)
+
+    retrained_score = integrative_utilities.read_training_statistics_file(retrained_reload_dir, 'test', 'r2_score')
+
+    print(f'original_score: {original_score:0.6f}, retrained_score: {retrained_score:0.6f}')
+    assert(np.absolute(retrained_score-original_score)<0.001), "retrained value different from original model"
+
+    # clean again
+    clean()
+
+
+
 def init():
     """
     Test full model pipeline: Curate data, fit model, and predict property for new compounds
@@ -139,4 +188,5 @@ def init():
 if __name__ == '__main__':
     test_RF()
     test_seeded_RF()
+    test_seeded_RF_retrain()
     #pass
