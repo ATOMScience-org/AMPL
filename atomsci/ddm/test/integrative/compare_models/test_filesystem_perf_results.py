da9e9c731c79dd492b4f9850f088903caf06a3e7
Jessica Mauvais <mauvais2@llnl.gov>
2024-04-11 09:59:42 -0700

    added different assert test for torch 2.2

--- atomsci/ddm/test/integrative/compare_models/test_filesystem_perf_results.py
+++ atomsci/ddm/test/integrative/compare_models/test_filesystem_perf_results.py
@@ -8,6 +8,7 @@ import tarfile
 import json
 import glob
 import pandas as pd
+import pdb
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../delaney_Panel'))
 from test_delaney_panel import init, train_and_predict
@@ -168,10 +169,19 @@ def test_XGB_results():
     clean()
     init()
     json_f = 'jsons/reg_config_delaney_fit_XGB_mordred_filtered.json'
+    pdb.set_trace()
 
     df1, df2, model_info = all_similar_tests(json_f)
 
-    assert json.loads(model_info['model_parameters_dict']) == {"xgb_gamma": 0.1,
+    model_info_dict = json.loads(model_info['model_parameters_dict'])
+    
+    # use different dict assert test for newer version of torch like 2.2
+    if len(model_info_dict.keys()) == 7:
+        assert json.loads(model_info['model_parameters_dict']) == {'xgb_colsample_bytree': 1.0,
+            'xgb_gamma': 0.1, 'xgb_learning_rate': 0.11, 'xgb_max_depth': 6,
+            'xgb_min_child_weight': 1.0, 'xgb_n_estimators': 100, 'xgb_subsample': 1.0}
+    elif len(model_info_dict.keys()) == 2:
+        assert json.loads(model_info['model_parameters_dict']) == {"xgb_gamma": 0.1,
             "xgb_learning_rate": 0.11,}
 
     assert model_info['feat_parameters_dict'] == json.dumps({})
