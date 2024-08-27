e007f80e66884460776a77ec18809462e4b7eab2
he6@llnl.gov <he6@llnl.gov>
2024-08-27 14:04:04 -0700

    Added some comments for this test and updated hte bad_model_tar.tar.gz file to contain scaled descriptors

--- atomsci/ddm/test/unit/test_model_file_reader.py
+++ atomsci/ddm/test/unit/test_model_file_reader.py
@@ -33,6 +33,7 @@ def test_no_medata_json_in_dir():
     assert e.type == IOError
 
 def test_multiple_models_metadata():
+    # these are correct tar.gz model files
     data_list =  mfr.get_multiple_models_metadata(
         os.path.join(currentdir, '../../examples/BSEP/models/bsep_classif_random_split.tar.gz'),
         os.path.join(currentdir, '../../examples/BSEP/models/bsep_classif_scaffold_split_graphconv.tar.gz'),
@@ -41,12 +42,14 @@ def test_multiple_models_metadata():
     assert len(data_list) == 3
     
 def test_incorrect_model_file():
+    # this file is not a tar.gz file
     with pytest.raises(Exception) as e:
         _ =  mfr.get_multiple_models_metadata(
             os.path.join(currentdir,'../../examples/BSEP/models/bsep_classif_random_split.tar'))
     assert e.type == IOError
 
 def test_bad_tar_file():
+    # this tar file contains scaled_descriptors. No model data at all.
     with pytest.raises(Exception) as e:
         _ =  ModelFileReader(
             os.path.join(currentdir, '../test_datasets/bad_model_tar.tar.gz'))
