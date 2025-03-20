b6b387e486dd86f6e10742dc3daa47b5b13da4d3
he6@llnl.gov <he6@llnl.gov>
2025-03-20 09:53:39 -0700

    Test dataset and featurizer creation independent of the model pipeline

--- atomsci/ddm/test/integrative/transfer_learning/test_embedding_and_feature.py
+++ atomsci/ddm/test/integrative/transfer_learning/test_embedding_and_feature.py
@@ -7,6 +7,7 @@ import atomsci.ddm.pipeline.compare_models as cp
 import atomsci.ddm.pipeline.featurization as feat
 import atomsci.ddm.pipeline.model_datasets as md
 import atomsci.ddm.utils.model_file_reader as mfr
+import tempfile
 import shutil
 import glob
 
@@ -104,6 +105,49 @@ def test_embedded_features():
 
     print('cleaned and done')
 
+def test_dataset_and_featurization_creation():
+    id_col = 'compound_id'
+    smiles_col = 'rdkit_smiles'
+    response_cols = 'activity'
+    dskey = os.path.join(os.path.dirname(__file__), 
+                         '../../test_datasets/H1_hybrid.csv')
+    model_path = os.path.join(os.path.dirname(__file__), 'embedding_model_ecfp.tar.gz')
+    result_dir = tempfile.mkdtemp()
+
+    transfer_json = {
+        "dataset_key" : dskey,
+        "datastore" : "False",
+        "uncertainty": "False",
+        "splitter": 'scaffold',
+        "split_valid_frac": "0.15",
+        "split_test_frac": "0.15",
+        "split_strategy": "train_valid_test",
+        "prediction_type": "regression",
+        "model_choice_score_type": "r2",
+        "response_cols" : response_cols,
+        "id_col": id_col,
+        "smiles_col" : smiles_col,
+        "result_dir": result_dir,
+        "system": "LC",
+        "transformers": "True",
+        "model_type": "NN",
+        "featurizer": "embedding",
+        "embedding_model_path": model_path,
+        "embedding_and_features": True,
+        "learning_rate": .0001,
+        "layer_sizes": [64, 64, 64],
+        "dropouts": [0.2, 0.2, 0.2],
+        "max_epochs": "5",
+        "early_stopping_patience": "5",
+        "verbose": "False"
+    }
+    pparams = parse.wrapper(transfer_json)
+
+    featurizer = feat.EmbeddingFeaturization(pparams)
+    dataset = md.create_model_dataset(pparams, featurizer)
+
+    assert featurizer.get_feature_count() == 1042
 
 if __name__ == '__main__':
+    test_dataset_and_featurization_creation()
     test_embedded_features()
