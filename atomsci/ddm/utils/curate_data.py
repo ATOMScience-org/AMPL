b825223bacf4ec695d173b52415c6a2476f02dfd
Amanda Paulson <amanda.paulson@ucsf.edu>
2026-01-29 12:44:03 -0800

    fix remove_outlier_replicates to dismiss NaN rows from response column before removing outliers

--- atomsci/ddm/utils/curate_data.py
+++ atomsci/ddm/utils/curate_data.py
@@ -522,7 +522,12 @@ def remove_outlier_replicates(df, response_col='pIC50', id_col='compound_id', ma
         result_df (DataFrame): Filtered data frame with outlier replicates removed.
 
     """
-
+    
+    prev_len = len(df)
+    df=df.dropna(subset=[response_col])
+    # log.info(f"Removed {prev_len - len(df)} rows with missing {response_col} values")
+    print(f"Removed {prev_len - len(df)} rows with missing {response_col} values")
+    
     fr_df = freq_table(df, id_col, min_freq=2)
     rep_ids = fr_df[id_col].values.tolist()
     has_rep_df = df[df[id_col].isin(rep_ids)]
